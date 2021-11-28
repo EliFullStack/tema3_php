5310426d5cac92cb15560c33b5031582c08ffb54
unknown <elisabet.v.alvarez@gmail.com>
2021-11-28 15:53:22 +0100

    exercici 1

---
+++ php basics/exercici 1/index.php
@@ -0,0 +1,18 @@
+<?php
+//DEFINIENDO LAS VARIABLES
+$edad = 25; //variable de tipo integer
+$longitud = 103.2; //variable de tipo double
+$frase = "Comenzando con PHP."; //variable de tipo string
+$llueve = true; //variable de tipo boolean
+
+//IMPRIMIENDO POR PANTALLA
+echo "Variable de tipo entera: $edad";
+echo "</br>";
+echo "Variable de tipo double: $longitud";
+echo "</br>";
+echo "Variable de tipo string: $frase";
+echo "</br>";
+echo "Variable de tipo boolean: $llueve";
+
+
+?>
