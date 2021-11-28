905a40c404e928763a872342f93c285a0fb1cbb2
unknown <elisabet.v.alvarez@gmail.com>
2021-11-28 20:24:51 +0100

    exercici 5

---
+++ php basics/exercici 5/index5.php
@@ -0,0 +1,15 @@
+<?php
+//defino los arrays
+$pares = array (2,6,10,22,36);
+$impares = array (3,7,11);
+
+//agrego un nuevo valor a $impares
+$impares[] = 21;
+
+//fusiono los dos arrays
+$fusionados = array_merge ($pares,$impares);
+echo "<pre>"; //me permite ver de manera ordenada la información
+var_dump ($fusionados);//imprimo por pantalla el array resultante valor por valor y su tamaño
+
+
+?>
