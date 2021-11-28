b160cb86088305565ca724dc3ebbca6f7496936f
unknown <elisabet.v.alvarez@gmail.com>
2021-11-28 16:34:20 +0100

    exercici 2

---
+++ php basics/exercici 2/index2.php
@@ -0,0 +1,14 @@
+<?php
+$string1 = "Hello World!";
+echo $string1;
+echo "</br>";
+echo strtoupper ($string1);
+echo "</br>";
+echo strlen ($string1);
+echo "</br>";
+echo strrev ($string1);
+echo "</br>";
+$string2 = "Aquest és el curs de PHP.";
+echo "$string1 $string2";
+
+?>
